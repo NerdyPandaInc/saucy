00c4a5c9f1f4c876ab64bba54fe25ef2e2431be0
NerdyPandaInc <nerdypanda805@gmail.com>
2025-07-03 21:36:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+  const rotator = document.querySelector(".background-rotator");
+
+  const backgrounds = [
+    'images/202572165617309.png',
+    'images/202572165631575.png',
+    'images/202572165113979.png'
+  ];
+
+  let index = 0;
+
+  function rotateBackground() {
+    rotator.style.backgroundImage = `url(${backgrounds[index]})`;
+    index = (index + 1) % backgrounds.length;
+  }
+
+  rotateBackground(); // initial load
+  setInterval(rotateBackground, 8000); // change every 8 seconds
+});
